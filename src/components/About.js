11ed4bea23d5f78572d3c3c1bafd262eee0985f2
Divyesh Gangwal <divyeshgangwal@gmail.com>
2023-03-09 22:43:46 +0530

    change colortheme view

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 import  {connect}  from 'react-redux';
 import  {action}  from '../redux/action-creators/index';
 import store from '../redux/store';
-import { Radio } from 'antd';
+import { Radio, Switch } from 'antd';
 
 
 class About extends Component {
-  handleThemeChange = (event) =>{
-    store.dispatch(action.changeTheme(event.target.value));
+  handleThemeChange = (checked) =>{
+    const theme = checked ? 'dark' : 'light';
+    store.dispatch(action.changeTheme(theme));
   };
   handleColorChange = (event) =>{
     store.dispatch(action.changeColor(event.target.value));
@@ -18,29 +19,33 @@ class About extends Component {
         <div>
           <h2>About</h2>
             <div>
-              <Radio name="theme" value="light" checked={this.props.theme==='light'} onChange={this.handleThemeChange} > 
-                Light 
-              </Radio>
-              <Radio name="theme" value="dark" checked={this.props.theme==='dark'} onChange={this.handleThemeChange} >
-                Dark 
-              </Radio>
+              <h3>Theme -
+              <Switch
+              checked={this.props.theme === 'dark'}
+              onChange={this.handleThemeChange}
+            />
+            {this.props.theme === 'dark' ? 'Dark' : 'Light'} 
+              </h3>
+              
             </div>
             <div>
-              <Radio name="color" value="blue" checked={this.props.theme==='blue'} onChange={this.handleColorChange} > 
+            <Radio.Group defaultValue="blue" buttonStyle="solid">
+              <Radio.Button name="color" value="blue" checked={this.props.theme==='blue'} onChange={this.handleColorChange} > 
                 Blue 
-              </Radio>
-              <Radio name="color" value="red" checked={this.props.theme==='red'} onChange={this.handleColorChange} >
+              </Radio.Button>
+              <Radio.Button name="color" value="red" checked={this.props.theme==='red'} onChange={this.handleColorChange} >
                 Red 
-              </Radio>
-              <Radio name="color" value="orange" checked={this.props.theme==='orange'} onChange={this.handleColorChange} >
+              </Radio.Button>
+              <Radio.Button name="color" value="orange" checked={this.props.theme==='orange'} onChange={this.handleColorChange} >
                 Orange 
-              </Radio>
-              <Radio name="color" value="green" checked={this.props.theme==='green'} onChange={this.handleColorChange} >
+              </Radio.Button>
+              <Radio.Button name="color" value="green" checked={this.props.theme==='green'} onChange={this.handleColorChange} >
                 Green 
-              </Radio>
-              <Radio name="color" value="purple" checked={this.props.theme==='purple'} onChange={this.handleColorChange} >
+              </Radio.Button>
+              <Radio.Button name="color" value="purple" checked={this.props.theme==='purple'} onChange={this.handleColorChange} >
                 Purple
-              </Radio>
+              </Radio.Button>
+              </Radio.Group>
             </div>
         </div>
     );
