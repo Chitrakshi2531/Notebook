1607efdcdd9517ae4f40ab0c997680b53a912167
Divyesh Gangwal <divyeshgangwal@gmail.com>
2023-03-11 14:03:36 +0530

    session

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,58 @@
-const connectToMongo = require('./db');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const app = express();
-const cors = require('cors');
-const port = 3001;
+    const connectToMongo = require('./db');
+    const express = require('express');
+    const session = require('express-session')
+    const MongoDBStore = require('connect-mongodb-session')(session);
+    const mongoose = require('mongoose');
+    const bodyParser = require('body-parser');
+    const app = express();
+    const mongoURI = "mongodb://dbGautum:Kamlesh@ac-k33on3k-shard-00-00.entrtz5.mongodb.net:27017,ac-k33on3k-shard-00-01.entrtz5.mongodb.net:27017,ac-k33on3k-shard-00-02.entrtz5.mongodb.net:27017/notebook?ssl=true&replicaSet=atlas-agjg2h-shard-0&authSource=admin&retryWrites=true&w=majority";
+    const cookieParser = require('cookie-parser')
+    const cors = require('cors')
+    const port = 3001;
 
-connectToMongo();
+    const store = new MongoDBStore({
+        uri: mongoURI,
+        collection: 'sessions'
+    });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({
-extended:true
-}));
+    connectToMongo();
 
-app.use('/auth',require('./routes/auth'));
-app.use('/notes',require('./routes/notes'));
+    app.use(session({
+        secret: 'chiku',
+        resave: false,
+        saveUninitialized: true,
+        store,
+    }));
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-});
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(cors());
+    app.use(cookieParser());
+    app.use(express.urlencoded({
+        extended:true
+    }));
+
+    app.use('/auth',require('./routes/auth'));
+    app.use('/notes',require('./routes/notes'));
+
+    app.get('/set-cookie', (req, res) => {
+        res.cookie('cookieName1', 'co44', { maxAge: 900000, httpOnly: false, secure: true });
+        console.log('Cookie has been set:', req.cookies);
+        res.send('Cookie has been set');
+      });
+      
+      app.get('/get-cookie', (req, res) => {
+        console.log('Cookies:', req.cookies);
+        res.send(req.cookies);
+      });
+      fetch('http://localhost:3001/get-cookie')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+  
+  
+    
+
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}` )
+    });
